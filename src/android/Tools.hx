8aaf80748d45bc2a4ead8c2db818eba17ba565d4
Cloudy <idk6291@gmail.com>
2022-07-12 14:39:24 +0200

    sure

--- src/android/Tools.hx
+++ src/android/Tools.hx
@@ -3,18 +3,25 @@ package android;
 import lime.system.JNI;
 
 /**
- * Some functions (not all) ported from a java class to haxe using jni
- *
  * @author 	Saw (M.A. Jigsaw)
  */
 class Tools
 {
 	/**
-	 * Return's the file you put as a file with url like from this (/storage/emulated/0/Documents/file.txt) to (file:///storage/emulated/0/Documents/file.txt).
+	 * Return's the file path you gave it but with a url like this (/storage/emulated/0/Documents/file.txt) to this (file:///storage/emulated/0/Documents/file.txt).
 	 */
 	public static function getFileUrl(path:String):String
 	{
 		var getFileUrl_jni = JNI.createStaticMethod("org/haxe/extension/Tools", "getFileUrl", "(Ljava/lang/String;)Ljava/lang/String;");
 		return getFileUrl_jni(path);
+	}
+
+	/**
+	 * Loads the system library specified by the libname argument.
+	 */
+	public static function loadLibrary(libname:String):Void
+	{
+		var loadLibrary_jni = JNI.createStaticMethod("java/lang/System", "loadLibrary", "(Ljava/lang/String;)V");
+		loadLibrary_jni(libname);
 	}
 }
--- src/android/os/Build.hx
+++ src/android/os/Build.hx
@@ -11,6 +11,11 @@ import lime.system.JNI;
 class Build
 {
 	/**
+	 * The name of the underlying board, like "goldfish".
+	 */
+	public static var BOARD:String = JNI.createStaticField("android/os/Build", "BOARD", "Ljava/lang/String;").get();
+
+	/**
 	 * The consumer-visible brand with which the product/hardware will be associated, if any.
 	 */
 	public static var BRAND:String = JNI.createStaticField("android/os/Build", "BRAND", "Ljava/lang/String;").get();
@@ -19,6 +24,21 @@ class Build
 	 * The name of the industrial design.
 	 */
 	public static var DEVICE:String = JNI.createStaticField("android/os/Build", "DEVICE", "Ljava/lang/String;").get();
+
+	/**
+	 * No Description.
+	 */
+	public static var HOST:String = JNI.createStaticField("android/os/Build", "HOST", "Ljava/lang/String;").get();
+
+	/**
+	 * Either a changelist number, or a label like "M4-rc20".
+	 */
+	public static var ID:String = JNI.createStaticField("android/os/Build", "ID", "Ljava/lang/String;").get();
+
+	/**
+	 * The name of the overall product.
+	 */
+	public static var PRODUCT:String = JNI.createStaticField("android/os/Build", "PRODUCT", "Ljava/lang/String;").get();
 
 	/**
 	 * The manufacturer of the product/hardware.
@@ -31,9 +51,29 @@ class Build
 	public static var MODEL:String = JNI.createStaticField("android/os/Build", "MODEL", "Ljava/lang/String;").get();
 
 	/**
+	 * No Description.
+	 */
+	public static var USER:String = JNI.createStaticField("android/os/Build", "USER", "Ljava/lang/String;").get();
+}
+
+class VERSION
+{
+	/**
+	 * The internal value used by the underlying source control to represent this build.
+	 * E.g., a perforce changelist number or a git hash.
+	 */
+	public static var INCREMENTAL:String = JNI.createStaticField("android/os/Build$VERSION", "INCREMENTAL", "Ljava/lang/String;").get();
+
+	/**
 	 * The user-visible version string.
 	 */
 	public static var RELEASE:String = JNI.createStaticField("android/os/Build$VERSION", "RELEASE", "Ljava/lang/String;").get();
+
+	/**
+	 * The user-visible SDK version of the framework in its raw String representation.
+	 */
+	@:deprecated("Use SDK_INT instead")
+	public static var SDK:String = JNI.createStaticField("android/os/Build$VERSION", "SDK", "Ljava/lang/String;").get();
 
 	/**
 	 * The SDK version of the software currently running on this hardware device.
